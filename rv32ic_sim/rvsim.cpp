665bf068ab2830243d6341acecb0733bb1ee9287
Ismail Shaheen <i.shaheen@aucegypt.edu>
2019-07-06 06:16:21 +0200

    16-bit Instructions support added

--- rv32ic_sim/rvsim.cpp
+++ rv32ic_sim/rvsim.cpp
@@ -28,7 +28,17 @@ void instDecExec(unsigned int instWord)
 	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
 	unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
 	unsigned int address;
-	unsigned int instPC = pc - 4;
+	unsigned int instPC;
+	unsigned char instSize;
+	if (instFlag) {
+		instPC = pc - 4;
+		instSize = 4;
+	}
+	else {
+		instPC = pc - 2;
+		instSize = 2;
+	}
+
 	if (instFlag) //32-bit instruction 
 	{
 		opcode = instWord & 0x0000007F;
@@ -48,39 +58,280 @@ void instDecExec(unsigned int instWord)
 	}
 	else
 	{
-		opcode = instWord & 0x0003;
-		funct3 = (instWord >> 15) & 0x7;
-		switch (opcode)
-		{
-		case 0:
-			if (funct3==2)
-			rd = (instWord >> 2) & 0x0007;
+		unsigned char quad = instWord & 0x3;
+		unsigned char cfunct3 = (instWord >> 13) & 0x7;
+		switch (quad) {
+		case 0: // Quadrant 0
+			opcode = 0x3;
+			funct3 = 2;
+			rs1 = (instWord >> 7) & 0x7;
+			I_imm = ((instWord >> 4) & 0x4) | ((instWord >> 7) & 0x38) | ((instWord << 1) & 0x40);
+			if (cfunct3 == 2) { // c.lw Instruction
+				rd = (instWord >> 2) & 0x7;
+			}
+			else if (cfunct3 == 6) { // c.sw Instruction
+				rs2 = (instWord >> 2) & 0x7;
+			}
+			else {
+				cout << "\tUnkown Quadrant 0 Compressed Instruction \n";
+				return;
+			}
 			break;
-		case 1:
+		case 1: // Quadrant 1
+			switch (cfunct3) {
+			case 0: {// c.addi Instrcution
+				opcode = 0x13;
+				rd = (instWord >> 7) & 0x1F;
+				funct3 = 0;
+				rs1 = rd;
+				I_imm = ((instWord >> 2) & 0x1F) | (((instWord >> 12) & 0x1) ? 0xFFFFFFE0 : 0x0);
+				break; }
+			case 1: // c.jal Instrcution
+				opcode = 0x6F;
+				rd = 1;
+				J_imm = ((instWord << 3) & 0x20) | ((instWord >> 2) & 0xE) | ((instWord << 1) & 0x80) | ((instWord >> 1) & 0x40) | ((instWord << 2) & 0x400) | ((instWord >> 1) & 0x300) | ((instWord >> 7) & 0x10) | (((instWord >> 12) & 0x1) ? 0xFFFFF800 : 0x0);
+				break;
+			case 2: // c.li Instruction
+				opcode = 0x13;
+				rd = (instWord >> 7) & 0x1F;
+				if (rd == 0) {
+					cout << "\tHINTs Instruction Detected\n";
+					return;
+				}
+				funct3 = 0;
+				rs1 = 0;
+				I_imm = ((instWord >> 2) & 0x1F) | (((instWord >> 12) & 0x1) ? 0xFFFFFFE0 : 0x0);
+				break;
+			case 3:
+				rd = (instWord >> 7) & 0x1F;
+				if (rd == 2) { // c.addi16sp Instruction
+					opcode = 0x13;
+					funct3 = 0;
+					rs1 = rd;
+					I_imm = ((instWord << 3) & 0x20) | ((instWord << 4) & 0x180) | ((instWord << 1) & 0x40) | ((instWord >> 2) & 0x10) | (((instWord >> 12) & 0x1) ? 0xFFFFFE00 : 0x0);
+				}
+				else if (rd == 0) {
+					cout << "\tHINTs Instruction Detected\n";
+					return;
+				}
+				else { // c.lui Instruction
+					opcode = 0x37;
+					U_imm = ((instWord << 10) & 0x1F000) | (((instWord >> 12) & 0x1) ? 0xFFFE0000 : 0x0);
+				}
+				break;
+			case 4: {
+				unsigned char cfunct2 = ((instWord >> 10) & 0x3);
+				switch (cfunct2) {
+				case 0: // c.srli
+					opcode = 0x13;
+					rd = (instWord >> 7) & 0x7;
+					funct3 = 5;
+					rs1 = rd;
+					I_imm = ((instWord >> 2) & 0x1F) | ((instWord >> 7) & 0x20);
+					break;
+				case 1: // c.srai
+					opcode = 0x13;
+					rd = (instWord >> 7) & 0x7;
+					funct3 = 5;
+					rs1 = rd;
+					I_imm = ((instWord >> 2) & 0x1F) | ((instWord >> 7) & 0x20) | 0x40000000;
+					break;
+				case 2: // c.andi
+					opcode = 0x13;
+					rd = (instWord >> 7) & 0x7;
+					funct3 = 7;
+					rs1 = rd;
+					I_imm = ((instWord >> 2) & 0x1F) | (((instWord >> 12) & 0x1) ? 0xFFFFFFE0 : 0x0);
+					break;
+				case 3:
+					if ((instWord >> 12) & 0x1) {
+						cout << "\tReserved Instruction Detected\n";
+						return;
+					}
+					else {
+						unsigned char cfunct2_2 = ((instWord >> 5) & 0x3);
+						opcode = instWord & 0x33;
+						rd = (instWord >> 7) & 0x7;
+						rs1 = rd;
+						rs2 = (instWord >> 2) & 0x7;
+						switch (cfunct2_2) {
+						case 0: // c.sub
+							funct3 = 0;
+							funct7 = 32;
+							break;
+						case 1: // c.xor
+							funct3 = 4;
+							funct7 = 0;
+							break;
+						case 2: // c.or
+							funct3 = 6;
+							funct7 = 0;
+							break;
+						case 3: // c.and
+							funct3 = 7;
+							funct7 = 0;
+							break;
+						default:
+							cout << "\tUnkown Quadrant 1 Compressed Instruction (cfunct3 = 0x3, cfunct2 = 0x3)\n";
+							return;
+							break;
+						}
+					}
+					break;
+				default:
+					cout << "\tUnkown Quadrant 1 Compressed Instruction (cfunct3 = 0x3)\n";
+					return;
+					break;
+				}
+				break;
+			}
+			case 5: // c.j Instruction
+				opcode = 0x6F;
+				rd = 0;
+				J_imm = ((instWord << 3) & 0x20) | ((instWord >> 2) & 0xE) | ((instWord << 1) & 0x80) | ((instWord >> 1) & 0x40) | ((instWord << 2) & 0x400) | ((instWord >> 1) & 0x300) | ((instWord >> 7) & 0x10) | (((instWord >> 12) & 0x1) ? 0xFFFFF800 : 0x0);
+				break;
+			case 6: // c.beqz
+				opcode = 0x63;
+				funct3 = 0;
+				rs1 = ((instWord << 7) & 0x7);
+				rs2 = 0;
+				B_imm = ((instWord << 3) & 0x20) | ((instWord >> 2) & 0x6) | ((instWord << 1) & 0xC0) | ((instWord >> 7) & 0x18) | (((instWord >> 12) & 0x1) ? 0xFFFFFF00 : 0x0);
+				break;
+			case 7: // c.bnez
+				opcode = 0x63;
+				funct3 = 1;
+				rs1 = ((instWord << 7) & 0x7);
+				rs2 = 0;
+				B_imm = ((instWord << 3) & 0x20) | ((instWord >> 2) & 0x6) | ((instWord << 1) & 0xC0) | ((instWord >> 7) & 0x18) | (((instWord >> 12) & 0x1) ? 0xFFFFFF00 : 0x0);
+				break;
+			default:
+				cout << "\tUnkown Quadrant 1 Compressed Instruction \n";
+				return;
+				break;
+			}
 			break;
-		case 2:
+		case 2: // Quadrant 2
+			switch (cfunct3) {
+			case 0: // c.slli
+				opcode = 0x13;
+				rd = (instWord >> 7) & 0x1F;
+				funct3 = 1;
+				rs1 = rd;
+				I_imm = ((instWord >> 2) & 0x1F);
+				if ((rd | I_imm) == 0) {
+					cout << "\tHINTs Instruction Detected\n";
+					return;
+				}
+				if ((instWord >> 12) & 0x1) {
+					cout << "\tReserved Instruction Detected\n";
+					return;
+				}
+				break;
+			case 1:
+				cout << "\tUnkown Quadrant 2 Compressed Instruction \n";
+				return;
+				break;
+			case 2: // c.lwsp
+				opcode = 0x3;
+				rd = (instWord >> 7) & 0x1F;
+				funct3 = 2;
+				rs1 = 2;
+				I_imm = ((instWord << 4) & 0xC0) | ((instWord >> 2) & 0x1C) | ((instWord >> 7) & 0x20);
+				if (rd == 0) {
+					cout << "\tReserved Instruction Detected\n";
+					return;
+				}
+				break;
+			case 3:
+				cout << "\tUnkown Quadrant 2 Compressed Instruction \n";
+				return;
+				break;
+			case 4:
+				rs2 = ((instWord >> 2) & 0x1F);
+				if ((instWord >> 12) & 0x1) {
+					rs1 = ((instWord >> 7) & 0x1F);
+					if (rs1 == 0) {
+						cout << "\tHINTs Instruction Detected\n";
+						return;
+					}
+					if (rs2 == 0) { // c.jalr
+						opcode = 0x67;
+						rd = 1;
+						I_imm = 0;
+					}
+					else { // c.add
+						opcode = 0x33;
+						funct3 = 0;
+						funct7 = 0;
+						rd = rs1;
+					}
+				}
+				else {
+					if (rs2 == 0) { // c.jr
+						opcode = 0x67;
+						rs1 = ((instWord >> 7) & 0x1F);
+						rd = 0;
+						I_imm = 0;
+						/*if (rs1 == 0) {
+							cout << "\tReserved Instruction Detected\n";
+							return;
+						}*/
+					}
+					else { // c.mv
+						opcode = 0x33;
+						funct3 = 0;
+						funct7 = 0;
+						rd = ((instWord >> 7) & 0x1F);
+						rs1 = 0;
+						if (rd == 0) {
+							cout << "\tHINTs Instruction Detected\n";
+							return;
+						}
+					}
+				}
+				break;
+			case 5:
+				cout << "\tUnkown Quadrant 2 Compressed Instruction \n";
+				return;
+				break;
+			case 6: // c.swsp
+				opcode = 0x23;
+				funct3 = 2;
+				rs1 = 2;
+				rs2 = ((instWord >> 2) & 0x1F);
+				S_imm = ((instWord >> 1) & 0xC0) | ((instWord >> 7) & 0x3C);
+				break;
+			case 7:
+				cout << "\tUnkown Quadrant 2 Compressed Instruction \n";
+				return;
+				break;
+			default:
+				cout << "\tUnkown Quadrant 2 Compressed Instruction \n";
+				return;
+				break;
+			}
 			break;
 		default:
+			cout << "\tUnkown Compressed Instruction \n";
+			return;
 			break;
 		}
-		
-
 	}
 
-	
 	printPrefix(instPC, instWord);
 
 	if (opcode == 0x33) {		// R Instructions
 		switch (funct3) {
-		case 0: if (funct7 == 32) {
+		case 0:
+			if (funct7 == 32) {
 			cout << "\tsub\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //Sub instruction 
 			regs[rd] = regs[rs1] - regs[rs2];
-		}
-				else {
-					cout << "\tadd\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //Add Instruction 
-					regs[rd] = regs[rs1] + regs[rs2];
-				}
-				break;
+			}
+			else {
+				cout << "\tadd\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //Add Instruction 
+				regs[rd] = regs[rs1] + regs[rs2];
+			}
+			break;
 
 		case 1:
 			cout << "\tsll\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //sll instruction 
@@ -93,7 +344,7 @@ void instDecExec(unsigned int instWord)
 			break;
 		case 3:
 			cout << "\tsltu\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //sltu instruction
-			unsigned int r1, r2;
+			unsigned int r1, r2; // TODO: remove unnecessary variables
 			r1 = regs[rs1];
 			r2 = regs[rs2];
 			if (r1 < r2) regs[rd] = 1;
@@ -104,13 +355,11 @@ void instDecExec(unsigned int instWord)
 			regs[rd] = regs[rs1] ^ regs[rs2];
 			break;
 		case 5:
-			if (funct7 == 32)
-			{
+			if (funct7 == 32) {
 				cout << "\tsra\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //sra instruction
 				regs[rd] = regs[rs1] >> regs[rs2];
 			}
-			else
-			{
+			else {
 				cout << "\tsrl\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n"; //srl instruction
 				regs[rd] = regs[rs1] >> regs[rs2];
 			}
@@ -125,11 +374,13 @@ void instDecExec(unsigned int instWord)
 			break;
 		default:
 			cout << "\tUnkown R Instruction \n";
+			break;
 		}
 	}
 	else if (opcode == 0x13) {	// I instructions
 		switch (funct3) {
-		case 0:	cout << "\taddi\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n"; //addi instruction
+		case 0:
+			cout << "\taddi\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n"; //addi instruction
 			regs[rd] = regs[rs1] + (int)I_imm;
 			break;
 		case 1:
@@ -144,7 +395,7 @@ void instDecExec(unsigned int instWord)
 			break;
 		case 3:
 			cout << "\tsltiu\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n"; //sltiu instruction
-			unsigned int r1;
+			unsigned int r1; // TODO: remove unnessary variables
 			r1 = regs[rs1];
 			if (r1 < I_imm) regs[rd] = 1;
 			else regs[rd] = 0;
@@ -162,7 +413,7 @@ void instDecExec(unsigned int instWord)
 			else
 			{
 				cout << "\tsrai\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n"; //srai instruction
-				int shamt;
+				int shamt; // TODO: remove unnessary variables
 				shamt = I_imm & 0x1F; //get the least siginificant 5 bits of the I_imm as the shift value 
 				regs[rd] = regs[rs1] >> shamt;
 
@@ -178,14 +429,14 @@ void instDecExec(unsigned int instWord)
 			break;
 		default:
 			cout << "\tUnkown I Instruction \n";
+			break;
 		}
 	}
 	else if (opcode == 0x3) { //I instructions - Load
-		unsigned int address;
-		switch (funct3) {
+		unsigned int address; // TODO: remove unnessary variables
+		switch (funct3) { // TODO: make hex with constatnt width
 		case 0:
 			cout << "\tlb\tx" << rd << ", " << hex << "0x" << (int)I_imm << "(x" << rs1 << ')' << "\n"; //lb instruction
-
 			address = I_imm +(unsigned int) regs[rs1];
 			regs[rd] = (unsigned char)memory[address] | ((memory[address]) >> 7 ? 0xFFFFFF00 : 0x0);
 			break;
@@ -214,15 +465,16 @@ void instDecExec(unsigned int instWord)
 			break;
 		default:
 			cout << "\tUnkown I Instruction \n";
+			break;
 		}
 	}
 	else if (opcode == 0x67) { //jalr instruction 
-		cout << "\tjalr\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << (int)I_imm << "\n";
-		regs[rd] = pc + 4;
-		pc = regs[rs1] + I_imm;
+		cout << "\tjalr\tx" << rd << ", x" << rs1 << ", " << hex << "0x" << std::setw(8) << (int)I_imm << "\n";
+		regs[rd] = pc;
+		pc = regs[rs1] + (int)I_imm;
 	}
 	else if (opcode == 0x23) {	// S Instruction
-		switch (funct3) {
+		switch (funct3) { // TODO: make immediate hex
 		case 0:
 			cout << "\tsb\tx" << rs2 << ", " << (int)S_imm << "(x" << rs1 << ")\n";
 			memory[(unsigned int)(regs[rs1] + (int)S_imm)] = char(regs[rs2] & 0x0FF);
@@ -239,36 +491,38 @@ void instDecExec(unsigned int instWord)
 			break;
 		default:
 			cout << "\tUnkown S Instruction \n";
+			break;
 		}
 	}
 	else if (opcode == 0x63) {	// B Instruction
 		switch (funct3) {
 		case 0:
 			cout << "\tbeq\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if (regs[rs1] == regs[rs2]) pc = pc + (int)B_imm - 4;
+			if (regs[rs1] == regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		case 1:
 			cout << "\tbne\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if (regs[rs1] != regs[rs2]) pc = pc + (int)B_imm - 4;
+			if (regs[rs1] != regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		case 4:
 			cout << "\tblt\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if (regs[rs1] < regs[rs2]) pc = pc + (int)B_imm - 4;
+			if (regs[rs1] < regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		case 5:
 			cout << "\tbge\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if (regs[rs1] >= regs[rs2]) pc = pc + (int)B_imm - 4;
+			if (regs[rs1] >= regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		case 6:
 			cout << "\tbltu\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if ((unsigned int)regs[rs1] < (unsigned int)regs[rs2]) pc = pc + (int)B_imm - 4;
+			if ((unsigned int)regs[rs1] < (unsigned int)regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		case 7:
 			cout << "\tbgeu\tx" << rs1 << ", x" << rs2 << ", " << hex << "0x" << std::setw(8) << ((int)B_imm >> 1) << "\n" << dec;
-			if ((unsigned int)regs[rs1] >= (unsigned int)regs[rs2]) pc = pc + (int)B_imm - 4;
+			if ((unsigned int)regs[rs1] >= (unsigned int)regs[rs2]) pc = pc + (int)B_imm - instSize;
 			break;
 		default:
 			cout << "\tUnkown B Instruction \n";
+			break;
 		}
 	}
 	else if (opcode == 0x37 || opcode == 0x17) {	// U instruction
@@ -279,14 +533,14 @@ void instDecExec(unsigned int instWord)
 			break;
 		case 0x17:
 			cout << "\tauipc\tx" << rd << hex << ", 0x" << std::setw(8) << (int)U_imm << "\n" << dec;
-			regs[rd] = pc + (U_imm << 12);
+			regs[rd] = pc - instSize + (U_imm << 12);
 			break;
 		}
 	}
 	else if (opcode == 0x6F) {	// J Instruction
 		cout << "\tjal\tx" << rd << hex << ", 0x" << std::setw(8) << ((int)J_imm >> 1) << "\n" << dec;
-		regs[rd] = pc + 4;
-		pc = pc + J_imm - 4;
+		regs[rd] = pc;
+		pc = pc + (int)J_imm - instSize;
 	}
 	else if (opcode == 0x73) {	// System Instruction
 		if (!I_imm) {
@@ -316,7 +570,7 @@ void instDecExec(unsigned int instWord)
 				exitFlag = 1;
 				break;
 			default:
-				cout << "\tInvalid system instruction service number\n";
+				cout << "\tUnknown system instruction service number\n";
 				break;
 			}
 		}
@@ -345,26 +599,22 @@ int main(int argc, char *argv[]) {
 		inFile.seekg(0, inFile.beg);
 		if (!inFile.read(memory, fsize)) emitError("Cannot read from input file\n");
 
-		while (true) { //32-bit instructions
-			if (((unsigned char)memory[pc] & 0x3)==0x3)
-				{
+		while (true) {
+			regs[0] = 0;
+			if (((unsigned char)memory[pc] & 0x3) == 0x3) { // 32-bit instructions
 				instFlag = true; 
 				instWord = (unsigned char)memory[pc] |
-					   //if (((unsigned char)memory[pc] & 0x3)==0x3)
-					   //{
-			regs[0] = 0;
-			instWord = (unsigned char)memory[pc] |
-				(((unsigned char)memory[pc + 1]) << 8) |
-				(((unsigned char)memory[pc + 2]) << 16) |
-				(((unsigned char)memory[pc + 3]) << 24);
-				}
-			else //compressed instructions
-				{
-				instFlag = false; 
-				instWord = (unsigned char)memory[pc] |(((unsigned char)memory[pc + 1]) << 8);
-				}
+					(((unsigned char)memory[pc + 1]) << 8) |
+					(((unsigned char)memory[pc + 2]) << 16) |
+					(((unsigned char)memory[pc + 3]) << 24);
+				pc += 4;
+			}
+			else { // 16-bit instructions
+				instFlag = false;
+				instWord = (unsigned char)memory[pc] | (((unsigned char)memory[pc + 1]) << 8);
+				pc += 2;
+			}
 
-			pc += 4;
 			// remove the (pc == 512) part from the following line once you have a complete simulator
 			if (pc == 512 || exitFlag) break;			// stop when PC reached address 512
 			instDecExec(instWord);
@@ -373,7 +623,7 @@ int main(int argc, char *argv[]) {
 		// dump the registers
 		for (int i = 0; i < 32; i++)
 			cout << "x" << dec << i << ": \t" << "0x" << hex << std::setfill('0') << std::setw(8) << regs[i] << "\n" << dec;
-		// Only for debugging dump memory
+		// dump memory (only for debugging)
 		/*for (int i = 0; i < 16; i++)
 			cout << "x" << hex << std::setfill('0') << std::setw(8) << i << ": \t" << "0x" << hex << (int)memory[i] << "\n";*/
 
